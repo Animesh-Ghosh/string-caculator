2e9b60c79819473f8ff2573e9fd1aecec058a145
Animesh-Ghosh <animesh.ghosh@tuta.io>
2025-05-04 19:02:58 +0530

    feat: Encapsulate business-logic into a StringCalculator class

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,33 +1,53 @@
 require 'strscan'
 
-def add(numbers)
-  delimiter = ','
-  if (match = numbers.match(%r{\A//(?<delimiters>\W+)\n}))
-    delimiters = match[:delimiters]
-    # either delimiter is within '[]' or it is without
-    delimiter = if delimiters.match?(/\[\W+\]/)
-                  collect_delimiters delimiters
-                else
-                  delimiters
-                end
-  end
-  numbers = numbers.split(/[#{delimiter}\n]/)
-                   .map(&:to_i)
-  negative_numbers = numbers.filter(&:negative?)
-  raise "negative numbers not allowed #{negative_numbers.join(",")}" if negative_numbers.any?
+class StringCalculator
+  def initialize(numbers) = (@numbers = numbers)
 
-  numbers.reject { |n| n > 1000 }
-         .reduce 0, :+
+  class NegativeNumbersError < StandardError
+    def initialize(numbers)
+      super("negative numbers not allowed #{numbers.join(",")}")
+    end
+  end
+
+  def add
+    numbers = @numbers.split(/[#{delimiter}\n]/)
+                      .map(&:to_i)
+    negative_numbers = numbers.filter(&:negative?)
+    raise NegativeNumbersError, negative_numbers if negative_numbers.any?
+
+    numbers.reject { |n| n > 1000 }
+           .reduce 0, :+
+  end
+
+  private
+
+  def delimiter
+    delimiter = ','
+    if (match = @numbers.match(%r{\A//(?<delimiters>\W+)\n}))
+      delimiters = match[:delimiters]
+      # either delimiter is within '[]' or it is without
+      delimiter = if delimiters.match?(/\[\W+\]/)
+                    collect_delimiters delimiters
+                  else
+                    delimiters
+                  end
+    end
+    delimiter
+  end
+
+  def collect_delimiters(delimiters)
+    s = StringScanner.new delimiters
+    delimiters = ''
+    until s.eos?
+      s.scan(/\[/)
+      s.scan(/(?<delimiter>[^\]]+)/)
+      delimiters << s[:delimiter]
+      s.scan(/\]/)
+    end
+    delimiters
+  end
 end
 
-def collect_delimiters(delimiters)
-  s = StringScanner.new delimiters
-  delimiters = ''
-  until s.eos?
-    s.scan(/\[/)
-    s.scan(/(?<delimiter>[^\]]+)/)
-    delimiters << s[:delimiter]
-    s.scan(/\]/)
-  end
-  delimiters
+def add(numbers)
+  StringCalculator.new(numbers).add
 end
